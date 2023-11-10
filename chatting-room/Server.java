7f2381b10211760771d14ba5d3cfb18f62798963
Manasvi Verma <manasviverma09@gmail.com>
2023-11-10 22:22:08 +0530

    final
    /s

--- chatting-room/Server.java
+++ chatting-room/Server.java
@@ -1,168 +1,170 @@
 import java.net.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import java.awt.BorderLayout;
-import java.awt.Font;
+import javax.swing.*;
+import java.awt.*;
 import java.io.*;
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
+import ai.openai.ChatCompletion;
+import ai.openai.ChatCompletionResponse;
 
-public class Server extends JFrame{//constructor
-
+public class Server extends JFrame {
     ServerSocket server;
     Socket socket;
-    BufferedReader br;//input stream
-    PrintWriter out; //output stream
+    BufferedReader br; // Input stream
+    PrintWriter out; // Output stream
 
     private JLabel heading = new JLabel("Server Area");
     private JTextArea messageArea = new JTextArea();
     private JTextField messageInput = new JTextField();
-    private Font font = new Font("Roboto",Font.PLAIN,20);
+    private Font font = new Font("Roboto", Font.PLAIN, 20);
 
-    public Server(){
-        try{
+    private JRadioButton chatGptRadio;
+    private JRadioButton chatClientRadio;
+
+    private boolean chattingWithGpt = false;
+
+    // Replace with your OpenAI API key
+    private String apiKey = "YOUR_OPENAI_API_KEY";
+    private String chatGptEngine = "text-davinci-002"; // Current model of ChatGPT API
+
+    public Server() {
+        try {
             server = new ServerSocket(7777);
-            System.out.println("waiting...");
-            socket=server.accept();
+            System.out.println("Waiting for a client...");
+            socket = server.accept();
 
-            //extracting input stream from socket; giving it to inputstream which will change the byte data received into character;
-            //bufferhandling will be done by BufferedReader; which will be then passed on to the var br. => br var will be used for input reading now on
             br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        
-            //to fetch output
             out = new PrintWriter(socket.getOutputStream());
 
             createGUI();
             handleEvents();
             startReading();
-            // startWriting();
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    } 
-    private void handleEvents(){
+    }
+
+    private void handleEvents() {
         messageInput.addKeyListener(new KeyListener() {
             @Override
-            public void keyPressed(KeyEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
+            public void keyPressed(KeyEvent e) {}
+
             @Override
             public void keyReleased(KeyEvent e) {
-                // TODO Auto-generated method stub
-                if(e.getKeyCode()==10){
-                    // System.out.println("you have pressed enter button");
-                    String contentToSend=messageInput.getText();
-                    messageArea.append("Me: " + contentToSend+"\n");
-                    out.println(contentToSend);
-                    out.flush();
+                if (e.getKeyCode() == 10) {
+                    String contentToSend = messageInput.getText();
+                    messageArea.append("Me: " + contentToSend + "\n");
+
+                    if (chattingWithGpt) {
+                        // Send user's message to ChatGPT API
+                        String response = chatWithGpt(contentToSend);
+                        messageArea.append("ChatGPT: " + response + "\n");
+                    } else {
+                        // Send user's message to the client
+                        out.println(contentToSend);
+                        out.flush();
+                    }
+
                     messageInput.setText("");
                     messageInput.requestFocus();
                 }
             }
+
             @Override
-            public void keyTyped(KeyEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
-            
+            public void keyTyped(KeyEvent e) {}
+        });
+
+        chatGptRadio.addActionListener(e -> {
+            chattingWithGpt = true;
+            messageArea.append("Now chatting with ChatGPT...\n");
+        });
+
+        chatClientRadio.addActionListener(e -> {
+            chattingWithGpt = false;
+            messageArea.append("Now chatting with the client...\n");
         });
     }
-    private void createGUI(){
-        this.setTitle("Server Messager"); 
-        this.setSize(600,700);
+
+    private void createGUI() {
+        this.setTitle("Server Messager");
+        this.setSize(600, 700);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
 
-
-        //coding for component 
         heading.setFont(font);
         messageArea.setFont(font);
         messageInput.setFont(font);
 
         heading.setHorizontalAlignment(SwingConstants.CENTER);
-        heading.setIcon(new ImageIcon("icons8-message-100.png"));
         heading.setHorizontalTextPosition(SwingConstants.CENTER);
-        
-        heading.setVerticalTextPosition(SwingConstants.BOTTOM);      
+        heading.setVerticalTextPosition(SwingConstants.BOTTOM);
         heading.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         messageArea.setEditable(false);
         messageInput.setHorizontalAlignment(SwingConstants.CENTER);
 
+        chatGptRadio = new JRadioButton("Chat with ChatGPT");
+        chatGptRadio.setFont(font);
+        chatGptRadio.setHorizontalAlignment(SwingConstants.CENTER);
 
-        //frame layout set
+        chatClientRadio = new JRadioButton("Chat with Client");
+        chatClientRadio.setFont(font);
+        chatClientRadio.setHorizontalAlignment(SwingConstants.CENTER);
+        chatClientRadio.setSelected(true);
+
+        ButtonGroup radioGroup = new ButtonGroup();
+        radioGroup.add(chatGptRadio);
+        radioGroup.add(chatClientRadio);
+
         this.setLayout(new BorderLayout());
-        //adding components to frame
-        this.add(heading,BorderLayout.NORTH);
-        JScrollPane jScrollPane=new JScrollPane(messageArea);
-        this.add(jScrollPane,BorderLayout.CENTER);
-        this.add(messageInput,BorderLayout.SOUTH);
-        
+        this.add(heading, BorderLayout.NORTH);
+        JScrollPane jScrollPane = new JScrollPane(messageArea);
+        this.add(jScrollPane, BorderLayout.CENTER);
+        this.add(messageInput, BorderLayout.SOUTH);
+        this.add(chatClientRadio, BorderLayout.SOUTH);
+        this.add(chatGptRadio, BorderLayout.SOUTH);
         heading.setLayout(new BorderLayout());
         this.setVisible(true);
     }
-    public void startReading(){
-        //thread 1: to read data(
-        Runnable r1=()->{ //lambda expression
-            System.out.println("reader started");
-            try{
-                while(true){
-                
-                        String msg = br.readLine();
-                        if(msg.equals("exit")){
-                            System.out.println("client terminated chat");
-                            JOptionPane.showMessageDialog(this, "client Terminanted the chat");
-                            messageInput.setEnabled(false);
-                            socket.close(); 
-                            break;
-                        }
-                        // System.out.println("Client : " + msg);
-                        messageArea.append("Client : "+ msg + "\n");       
+
+    public void startReading() {
+        Runnable r1 = () -> {
+            System.out.println("Reader started");
+            try {
+                while (true) {
+                    String msg = br.readLine();
+                    if (msg.equals("exit")) {
+                        System.out.println("Client terminated chat");
+                        JOptionPane.showMessageDialog(this, "Client Terminated the chat");
+                        messageInput.setEnabled(false);
+                        socket.close();
+                        break;
+                    }
+                    messageArea.append("Client: " + msg + "\n");
                 }
-            }catch(Exception e){
-                // e.printStackTrace();
-                 System.out.println("connection closed");
+            } catch (Exception e) {
+                System.out.println("Connection closed");
             }
         };
-        new Thread(r1).start();//thread obj intialization
+        new Thread(r1).start();
     }
-    public void startWriting(){
-        //thread 2: user intake data and send it to client
-        System.out.println("typing...   ");
-        Runnable r2=()->{
-            try{
-                while(true && !socket.isClosed()){
-                        //we have to write data on server; for that we have to take data from console
-                        BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
-                        String content = br1.readLine(); 
-                        
-                        out.println(content);
-                        out.flush();
-                        if(content.equals("exit")){
-                            socket.close();
-                            break;
-                        }
 
-                }
-            }catch(Exception e){
-                System.out.println("connection closed");
-            }
-             System.out.println("connection closed");
-        };
-
-        new Thread(r2).start();
+    private String chatWithGpt(String message) {
+        try {
+            ChatCompletion chatCompletion = new ChatCompletion(apiKey);
+            String chatPrompt = "You: " + message + "\nChatGPT:";
+            String response = chatCompletion.complete(chatPrompt, chatGptEngine);
+            return response;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "ChatGPT Error: " + e.getMessage();
+        }
     }
-    public static void  main(String[] args){
-        System.out.println("This is server going to start server");
-        new Server();//calling constructor
+
+    public static void main(String[] args) {
+        System.out.println("This is the server. Waiting for a client...");
+        new Server();
     }
 }
